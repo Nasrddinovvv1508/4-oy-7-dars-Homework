e93bfda1f66223004abd9f517e0b91de0c3f878b
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2023-12-21 21:18:32 +0500

    All done

---
+++ index.js
@@ -0,0 +1,101 @@
+    // 1-MASALA
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// function countArr(arg) {
+//     let counter = 0;
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 1) {
+//             counter++;
+//         }
+//     }
+
+//     return counter;
+// }
+
+// console.log(countArr(arr));
+
+
+
+
+
+    // 2-MASALA
+// let array = [3, 7, 21, 14, 21];
+
+// function multipleArray(arg) {
+//     let multiple = 1;
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 3 == 0 && arg[i] % 7 == 0) {
+//             multiple *= arg[i]
+//         }
+//     }
+
+//     return multiple;
+// }
+
+// console.log(multipleArray(array));
+
+
+
+
+
+    // 3-MASALA
+// let arr = [1, 2, 3, 4, 5];
+
+// function newArray(arg) {
+//     let new_array;
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 1) {
+//           new_array = arr.push(arg[i])
+//         }
+//     }
+
+//     return new_array;
+// }
+
+// console.log(newArray(arr));
+
+
+
+
+
+    // 4-MASALA
+// let arr = [1, 2, 3, 4, 5];
+
+// function newArray(arg) {
+//     let new_array;
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 0) {
+//           arr.push(arg[i])
+//         }
+//     }
+
+//     return new_array;
+// }
+
+// console.log(newArray(arr));
+
+
+
+
+    // 6-MASALA
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let counter = 0;
+
+// function resArray(arg) {
+//     let counterI = 0;
+//     for (let i = 0; i < arg.length; i++) {
+//         for (let j = 0; j < arg[i].length; j++) {
+//             if (arg[i] % j == 0) {
+//                 counterI++;
+//             }
+//         }
+//     }
+
+//     if (counterI == 2) {
+//         counter++;
+//     }
+
+//     return counter;
+// }
+
+// console.log(resArray(arr));
